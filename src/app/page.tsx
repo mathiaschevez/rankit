5bc0cdf58a276e749b37f23171d48e5b1525d0f1
Mathias Chevez <davidchevez90@gmail.com>
2025-01-24 04:05:33 -0700

    removed rankings component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
-// import { Suspense } from "react";
-// import Rankings from "./components/Rankings";
+import { getRankings } from "@/server/queries";
 
-export default function Home() {
+export default async function Home() {
+  const rankings = await getRankings();
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <NavBar />
-      {/* <Suspense fallback={<>loading...</>}>
-        <Rankings />
-      </Suspense> */}
+      {rankings.map((ranking) =>
+        <div
+          key={ranking.id}
+          className='border border-white'
+        >
+          {ranking.name}
+        </div>)
+      }
     </div>
   );
 }
