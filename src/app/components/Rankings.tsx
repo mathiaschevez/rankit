85a043b7d23ba385da60b313545b67b30c9ad7fe
Mathias Chevez <davidchevez90@gmail.com>
2025-01-24 03:23:39 -0700

    check for data

--- src/app/components/Rankings.tsx
+++ src/app/components/Rankings.tsx
@@ -4,12 +4,12 @@ import React from 'react'
 export default async function Rankings() {
   const rankings = await getRankings();
 
-  return rankings.map((ranking) =>
+  return rankings ? rankings.map((ranking) =>
     <div
       key={ranking.id}
       className='border border-white'
     >
       {ranking.name}
     </div>
-  )
+  ) : <></>
 }
