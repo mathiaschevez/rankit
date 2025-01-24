94ddf772ec7de14f708dc32caac229373b4041b2
Mathias Chevez <davidchevez90@gmail.com>
2025-01-24 03:54:52 -0700

    narrowing issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,19 @@
-import { Suspense } from "react";
-import Rankings from "./components/Rankings";
+// import { Suspense } from "react";
+// import Rankings from "./components/Rankings";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Suspense fallback={<>loading...</>}>
+      <NavBar />
+      {/* <Suspense fallback={<>loading...</>}>
         <Rankings />
-      </Suspense>
+      </Suspense> */}
     </div>
   );
+}
+
+function NavBar() {
+  return (
+    <div>Nav</div>
+  )
 }
