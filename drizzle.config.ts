8b240f6b6970abecabf36b6ca313edde129e4127
Mathias Chevez <davidchevez90@gmail.com>
2025-01-24 04:37:40 -0700

    added envjs

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,12 @@
-import { config } from 'dotenv';
-import { defineConfig } from 'drizzle-kit';
+import { defineConfig } from 'drizzle-kit'
+import { env } from "@/env";
 
-config({ path: '.env.local' });
 
 export default defineConfig({
   schema: './src/server/db/schema.ts',
   out: './supabase/migrations',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: env.DATABASE_URL,
   },
 });
