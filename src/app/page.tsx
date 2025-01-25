9622fbe4d7d5cdef1b911d2dbdd9e9d43ad8c7f5
Mathias Chevez <davidchevez90@gmail.com>
2025-01-24 22:35:06 -0700

    added shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default async function Home() {
   const rankings = await getRankings();
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="">
       <NavBar />
       {rankings.map((ranking) =>
         <div
