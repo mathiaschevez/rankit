060807e8d3f44bcd7f91368c6c67272f7df80e07
Mathias Chevez <davidchevez90@gmail.com>
2025-01-24 23:52:08 -0700

    added loading svg

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,3 +1,9 @@
+import { Loader } from "@/components/ui/loader";
+
 export default function Loading() {
-  return <>Loading...</>
+  return (
+    <div className="h-screen w-screen flex items-center justify-center">
+      <div className="h-24 w-24"><Loader /></div>
+    </div>
+  )
 }
