45cf2412b129c6ad982a41c042d41f4a879738fd
Mathias Chevez <davidchevez90@gmail.com>
2025-03-03 12:45:43 -0700

    update hostname

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import next from "next";
 import { Server } from "socket.io";
 
 const dev = process.env.NODE_ENV !== "production";
-const hostname = "localhost";
+const hostname = dev ? "localhost" : "https://rankit.cc"
 const port = 3000;
 // when using middleware `hostname` and `port` must be provided below
 const app = next({ dev, hostname, port });
