d74c817d7aff8a97fe924d4cdf3109f8f7c86cd1
Mathias Chevez <davidchevez90@gmail.com>
2025-03-24 06:47:12 -0600

    comment pending rank item for now

--- src/components/PendingRankItem.tsx
+++ src/components/PendingRankItem.tsx
@@ -1,41 +1,44 @@
 'use client';
 
-import { SelectPendingRankItem } from "@/server/db/schema";
-import Image from "next/image";
-import { Button } from "./ui/button";
-import { deletePendingRankItem, insertRankItems } from "@/server/queries";
-import { useRouter } from "next/navigation";
-import { useSelector } from "@/redux/store";
+// import { SelectPendingRankItem } from "@/server/db/schema";
+// import Image from "next/image";
+// import { Button } from "./ui/button";
+// import { deletePendingRankItem, insertRankItems } from "@/server/queries";
+// import { useRouter } from "next/navigation";
+// import { useSelector } from "@/redux/store";
 
-export default function PendingRankItem({ pendingRankItem, index, rankingUserId }: { pendingRankItem: SelectPendingRankItem, index: number, rankingUserId: string }) {
-  const router = useRouter();
-  const user = useSelector(state => state.user)
+// { pendingRankItem, index, rankingUserId }: { pendingRankItem: SelectPendingRankItem, index: number, rankingUserId: string }
 
-  function handleAcceptRankItem(pendingRankItem: SelectPendingRankItem) {
-    insertRankItems([{
-      name: pendingRankItem.name,
-      fileName: pendingRankItem.fileName,
-      imageKey: pendingRankItem.imageKey,
-      imageUrl: pendingRankItem.imageUrl,
-      rankingId: pendingRankItem.rankingId,
-    }]).then(() => {
-      deletePendingRankItem(pendingRankItem.id).then(() => router.refresh());
-    })
-  }
+export default function PendingRankItem() {
+  // const router = useRouter();
+  // const user = useSelector(state => state.user)
 
-  function handleDeclineRankitem(id: number) {
-    deletePendingRankItem(id).then(() => router.refresh());
-  }
+  // function handleAcceptRankItem(pendingRankItem: SelectPendingRankItem) {
+  //   insertRankItems([{
+  //     name: pendingRankItem.name,
+  //     fileName: pendingRankItem.fileName,
+  //     imageKey: pendingRankItem.imageKey,
+  //     imageUrl: pendingRankItem.imageUrl,
+  //     rankingId: pendingRankItem.rankingId,
+  //   }]).then(() => {
+  //     deletePendingRankItem(pendingRankItem.id).then(() => router.refresh());
+  //   })
+  // }
+
+  // function handleDeclineRankitem(id: number) {
+  //   deletePendingRankItem(id).then(() => router.refresh());
+  // }
 
   return (
     <div className='flex w-full h-20 items-center gap-4'>
-      <div>{index}</div>
+      Pending rank item
+      {/* <div>{index}</div>
       <Image alt="rankItemImage" src={pendingRankItem.imageUrl} width={50} height={50} />
       <div className='text-xl flex-1'>{pendingRankItem.name}</div>
       {user.user?.userId === rankingUserId ? <div className="flex gap-4">
         <Button onClick={() => handleDeclineRankitem(pendingRankItem.id)}>Decline</Button>
         <Button onClick={() => handleAcceptRankItem(pendingRankItem)}>Accept</Button>
-      </div> : <i className="text-slate-500">Pending</i>}
+      </div> : <i className="text-slate-500">Pending</i>} */}
     </div>
   )
 }
