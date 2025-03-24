6daf8fd26e9f1401b82f73533f2d4281de1a1301
Mathias Chevez <davidchevez90@gmail.com>
2025-03-24 06:40:43 -0600

    remove old db code / update envs

--- src/server/db/index.ts
+++
@@ -1,6 +0,0 @@
-import { env } from '@/env';
-import { drizzle } from 'drizzle-orm/postgres-js';
-import postgres from 'postgres';
-
-const client = postgres(env.DATABASE_URL);
-export const db = drizzle({ client });
